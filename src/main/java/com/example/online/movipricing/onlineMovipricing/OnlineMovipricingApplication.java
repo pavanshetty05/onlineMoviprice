842d082bdc56c0677a16ed0fb50c324c83719acb
ASUS <pavanshetty05@gmail.com>
2023-04-30 20:34:14 +0530

    Even trigger

--- src/main/java/com/example/online/movipricing/onlineMovipricing/OnlineMovipricingApplication.java
+++ src/main/java/com/example/online/movipricing/onlineMovipricing/OnlineMovipricingApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineMovipricingApplication {
+	
 
 	public static void main(String[] args) {
+		System.out.println("Even trigger branch");
 		SpringApplication.run(OnlineMovipricingApplication.class, args);
 	}
 
