936a055c39a74bccea31cc6fd002ccf4824e09b7
ASUS <pavanshetty05@gmail.com>
2023-04-30 22:14:29 +0530

    First copmmit

--- src/main/java/com/example/online/movipricing/onlineMovipricing/OnlineMovipricingApplication.java
+++ src/main/java/com/example/online/movipricing/onlineMovipricing/OnlineMovipricingApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineMovipricingApplication {
 	 public static void main(String[] args) {
 		 
-		 
 		SpringApplication.run(OnlineMovipricingApplication.class, args);
+		
 	}
 
 }
