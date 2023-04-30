d2b8d11025f438e41e8b460d758fb020f1cfe542
ASUS <pavanshetty05@gmail.com>
2023-04-30 11:32:11 +0530

    Second commit

---
+++ src/main/java/com/example/online/movipricing/onlineMovipricing/OnlineMovipricingApplication.java
@@ -0,0 +1,13 @@
+package com.example.online.movipricing.onlineMovipricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovipricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovipricingApplication.class, args);
+	}
+
+}
